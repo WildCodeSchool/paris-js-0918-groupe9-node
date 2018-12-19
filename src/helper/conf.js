439c54debcfdafd5f0899d37f9320ac08c3c1864
Unknown <hocamvan.htcv@gmail.com>
2018-12-19 09:18:17 +0100

    correction des routes

--- src/helper/conf.js
+++ src/helper/conf.js
@@ -2,8 +2,8 @@ const mysql = require ('mysql')
 
 const connection = mysql.createConnection({
     host:"localhost",
-    user:"root",
-    password:"Ironworks-239686",
+    user:"camvan",
+    password:"toto",
     database:"allsponsored"
 })
 
