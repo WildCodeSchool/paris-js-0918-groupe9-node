df638f37c514d79e00ce7e55faa6c65e1dd286b9
Unknown <hocamvan.htcv@gmail.com>
2019-01-10 11:47:54 +0100

    route_contract_has_product

--- src/routes/contract_has_product.js
+++ src/routes/contract_has_product.js
@@ -11,6 +11,18 @@ Router.get('/', (req, res) => {
         }
     });
 })
+Router.get('/contract-has-product', (req, res) => {
+    connection.query('select product.name from contract_has_product \
+    inner join contract on contract.id= contract_has_product.contract_id \
+    inner join product on product.id = contract_has_product.product_id \
+    where contract_id=?', req.body.contract_id,(err, results) => {
+        if (err) {
+            res.status(500).send('Erreur lors de la récupération des données');
+        } else {
+            res.json(results);
+        }
+    });
+})
 Router.get('/:id', (req, res) => {
     connection.query('SELECT * from contract_has_product where id=?', req.params.id, (err, results) => {
         if (err) {
