e9f6fc533ccc6d8c56d84492b62ae223c3ff4353
Unknown <hocamvan.htcv@gmail.com>
2019-01-10 10:15:19 +0100

    route_project_orderhasproduct

---
+++ src/routes/contract_has_product.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const connection = require('../helper/conf.js')
+const Router = express.Router();
+
+Router.get('/', (req, res) => {
+    connection.query('SELECT * from contract_has_product', (err, results) => {
+        if (err) {
+            res.status(500).send('Erreur lors de la récupération des données');
+        } else {
+            res.json(results);
+        }
+    });
+})
+Router.get('/:id', (req, res) => {
+    connection.query('SELECT * from contract_has_product where id=?', req.params.id, (err, results) => {
+        if (err) {
+            res.status(500).send("Erreur lors de la récupération des données");
+        } else {
+            res.json(results);
+        }
+    });
+})
+Router.post('/', (req, res) => {
+    connection.query('INSERT into contract_has_product SET ?', req.body, (err, results) => {
+        console.log(results);
+        if (err) {
+            console.log(err);
+            res.status(500).send(`Erreur lors de l'insertion des données`);
+        }
+        else {
+            res.sendStatus(200);
+            }
+        })
+});
+
+Router.put('/:id', (req, res) => {
+    const idaction = req.params.id;
+    const formData = req.body;
+    formData.updated_at = new Date();
+    connection.query('UPDATE contract_has_product SET ? WHERE id = ?', [formData, idaction], (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la modification des données");
+        } else {
+            res.sendStatus(200);
+        }
+
+    })
+})
+Router.delete('/:id', (req, res) => {
+    connection.query('DELETE FROM contract_has_product WHERE id =?', req.params.id, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la suppression");
+        } else {
+            res.sendStatus(200);
+        }
+
+    })
+})
+
+module.exports = Router;
